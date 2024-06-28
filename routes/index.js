482b67cdf226455316cd2810f0b5e9f391780b45
Irene <gallibour.irene@gmail.com>
2024-06-28 11:37:18 +0200

    test 1

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/year', (req, res) => {
+  const currentYear = new Date().getFullYear();
+  res.json({ year: currentYear })
+})
+;
+
+module.exports = router;
